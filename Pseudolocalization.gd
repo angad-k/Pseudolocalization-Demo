b21f645ef1ba5a4ecf5e207533341f2938b36f79
Angad Kambli <angadkambli@gmail.com>
2021-06-25 16:20:17 +0530

    add expansion features

--- Pseudolocalization.gd
+++ Pseudolocalization.gd
@@ -7,6 +7,7 @@ func _ready():
 	$HBoxContainer/Pseudolocalization_options/doublevowels.pressed = TranslationServer.is_pseudolocalization_double_vowels_enabled()
 	$HBoxContainer/Pseudolocalization_options/prefix/TextEdit.text = TranslationServer.get_pseudolocalization_prefix()
 	$HBoxContainer/Pseudolocalization_options/suffix/TextEdit.text = TranslationServer.get_pseudolocalization_suffix()
+	$HBoxContainer/Pseudolocalization_options/exp_ratio/TextEdit.text = str(TranslationServer.get_pseudolocalization_expansion_ratio())
 	pass 
 
 func _on_accents_toggled(button_pressed):
@@ -41,4 +42,16 @@ func _on_Pseudolocalize_pressed():
 
 func _on_doublevowels_toggled(button_pressed):
 	TranslationServer.set_pseudolocalization_double_vowels_enabled(button_pressed)
+	pass # Replace with function body.
+
+
+func _on_expansion_ratio_text_changed():
+	var ratio = ($HBoxContainer/Pseudolocalization_options/exp_ratio/TextEdit.text).to_float()
+	if ratio > 1:
+		ratio = 1
+		$HBoxContainer/Pseudolocalization_options/exp_ratio/TextEdit.text = str(ratio)
+	if ratio < 0:
+		ratio = 0 
+		$HBoxContainer/Pseudolocalization_options/exp_ratio/TextEdit.text = str(ratio)
+	TranslationServer.set_pseudolocalization_expansion_ratio(ratio)
 	pass # Replace with function body.
